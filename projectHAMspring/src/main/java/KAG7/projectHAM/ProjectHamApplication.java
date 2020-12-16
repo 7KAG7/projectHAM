8d1ce5b8745c98f8db6081ecf4db64aa543f6b1e
keith <guernsey.keith@gmail.com>
2020-12-16 09:24:09 -0500

    added maven

---
+++ projectHAMspring/src/main/java/KAG7/projectHAM/ProjectHamApplication.java
@@ -0,0 +1,13 @@
+package KAG7.projectHAM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectHamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectHamApplication.class, args);
+	}
+
+}
---
+++ projectHAMspring/src/test/java/KAG7/projectHAM/ProjectHamApplicationTests.java
@@ -0,0 +1,13 @@
+package KAG7.projectHAM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectHamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
